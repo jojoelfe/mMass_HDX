388339c9ebf9da906e13802648b283d3088657c2
jojoelfe <elferich@ohsu.edu>
2014-01-15 13:05:29 -0800

    Changes to script

--- mMass/scripts/extract_fragment.py
+++ mMass/scripts/extract_fragment.py
@@ -3,28 +3,34 @@ sys.path.append("../")
 import mspy
 import numpy as np
 import pylab as pl
-from matplotlib.collections import LineCollection
 
 #Load mzml file
 parser = mspy.parseMZML(
-    "C:\Users\jojot_000\Documents\\011114\FIMC_digest_test04_1_100_pepsin.mzML")
+    "/Volumes/FAT/011114/IMC_digest_test04_1_100_pepsin.mzML")
 
 parser.load()
 
 ScanList = parser.scanlist()
 ms1ScanList = []
+profiles = {}
 
 for key, scan_info in ScanList.iteritems():
     if scan_info['msLevel'] == 1:
         ms1ScanList.append(key)
+        profiles[key] = parser.scan(key).profile
 #Get fragment parameters
-fragment = "SLSPHRPRHSRLQREPQVQWL"
-charge = 4
 
 
-sequence_obj = mspy.sequence(fragment)
+parser = None
+
+fragment_list = ["SLSPHRPRHSRLQREPQVQWL"]
+charge_min = 1
+charge_max = 6
+
+
+sequence_obj = mspy.sequence(fragment_list[0])
 pattern_obj = mspy.pattern(sequence_obj.formula(),
-                           charge=charge, real=False)
+                           charge=4, real=False)
 #Perform calculations
 time = []
 basepeak = []
@@ -32,29 +38,28 @@ rmsd = []
 profile = None
 for scan_number in ms1ScanList:
     time.append(ScanList[scan_number]['retentionTime'])
-    if mspy.checkpattern(signal=parser.scan(scan_number).profile,
-                         pattern=pattern_obj) is not None:
-        rmsd.append(mspy.checkpattern(signal=parser.scan(scan_number).profile,
-                                      pattern=pattern_obj).rmsd)
-        basepeak.append(mspy.checkpattern(
-            signal=parser.scan(scan_number).profile,
-            pattern=pattern_obj).basepeak)
+    checkPatternResult = mspy.checkpattern(signal=profiles[scan_number],
+                                           pattern=pattern_obj)
+    if checkPatternResult is not None:
+        rmsd.append(checkPatternResult.rmsd)
+        basepeak.append(checkPatternResult.basepeak)
         if rmsd[-1] < 0.15:
             if profile is None:
-                profile = mspy.crop(parser.scan(scan_number).profile,
-                                    pattern_obj[0][0] - 0.1, pattern_obj[-1][0] + 0.1)
+                profile = mspy.crop(profiles[scan_number],
+                                    pattern_obj[0][0] - 0.1,
+                                    pattern_obj[-1][0] + 0.1)
             else:
                 profile = mspy.combine(profile, mspy.crop(
-                    parser.scan(scan_number).profile, pattern_obj[0][0] - 0.1,
+                    profiles[scan_number], pattern_obj[0][0] - 0.1,
                     pattern_obj[-1][0] + 0.1))
     else:
         rmsd.append(1)
         basepeak.append(0)
 profile = mspy.reduce(profile)
 
-pl.plot(time,rmsd)
+pl.plot(time, rmsd)
 pl.show()
-
+pl.plot(time, basepeak)
 pl.show()
 pl.plot(*profile.T)
 pl.show()
